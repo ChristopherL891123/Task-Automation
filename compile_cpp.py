a7f2cd6c233d912550095c2af3544e7f394bfaef
Lama, Christopher <77355477+ChristopherL891123@users.noreply.github.com>
2023-05-08 20:27:33 -0500

    Add files via upload

---
+++ compile_cpp.py
@@ -0,0 +1,49 @@
+# Christopher Lama
+import subprocess
+import traceback
+import os
+import inquirer as iq
+
+try:
+    final_file_name = input("NAME OF EXE: ")
+    choice = input("COMPILE ALL OR SOME (a/s) : ")
+    pwd_dir = os.getcwd() # get current working directory: https://stackoverflow.com/questions/37427683/python-search-for-a-file-in-current-directory-and-all-its-parents
+    cpp_file_list = []
+
+    if choice.lower() == "all":
+
+        for files in os.listdir(pwd_dir):
+            if files.endswith(".cpp") or files.endswith(".h"):
+                cpp_file_list.append(os.path.join(pwd_dir, files))
+                print(cpp_file_list)
+
+
+        command = ["g++"] + cpp_file_list + ["-o", os.path.join(pwd_dir, final_file_name + ".exe")]
+        subprocess.run(command,shell=False)
+        input("Press any key to exit ...")
+
+    if choice.lower() == "some":
+        if choice.lower() == "some":
+
+            file_list = []
+            for file in os.listdir(os.getcwd()):
+                if file.endswith(".cpp") or file.endswith(".h"):
+                    file_list.append(os.path.abspath(os.path.join(pwd_dir, file)))
+
+            questions = [iq.Checkbox('file',
+                                     message="Choose file path(s): ",
+                                     choices=file_list), ]
+
+            answers = iq.prompt(questions)
+            selected_files = answers['file']
+            files_string = " ".join(selected_files)
+            command = ["g++"] + selected_files + ["-o", os.path.join(pwd_dir, final_file_name + ".exe")]
+            subprocess.run(command, shell=False)
+            input("Press any key to exit ...")
+
+
+
+except:
+    traceback.print_exc()
+    input("Press any key to exit ...")
+
